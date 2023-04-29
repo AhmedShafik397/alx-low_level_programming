cdcd7ffcb6b9306bf4b60c76fb4b96ef1026bdec
Ahmed Gaber <you@example.com>
2023-04-29 06:51:07 -0700

    commit

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
-
-// main - prints the name of the file
-
-int main(void)
-{
-		printf("%s\n", __FILE__);
-			return (0);
+/**
+ * *Main -Print name of this file
+ *
+ * Return:Always 0.
+ */
+int main(void){
+	printf("%s\n", __FILE__);
+	return (0);
 }
