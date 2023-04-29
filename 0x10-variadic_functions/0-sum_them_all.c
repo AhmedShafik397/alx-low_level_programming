f731956953d6fc7be03aca76154cc8c4bcd2d258
Ahmed Gaber <you@example.com>
2023-04-29 06:22:20 -0700

    commit

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -12,7 +12,8 @@
 int sum_them_all(const unsigned int n, ...)
 {
 		va_list ap;
-			unsigned int i, sum = 0;
+
+		unsigned int i, sum = 0;
 
 				va_start(ap, n);
 
@@ -21,5 +22,6 @@ int sum_them_all(const unsigned int n, ...)
 
 						va_end(ap);
 
-							return (sum);
+
+						return (sum);
 }
